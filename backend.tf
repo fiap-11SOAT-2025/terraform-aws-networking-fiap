a781e53b91702e82af535a96bc30971328501ff6
AldoOps <aldo.santos.oliveira95@gmail.com>
2025-11-23 21:31:15 -0300

    criar networking

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fiap-terraform-state-bucket-fiap-aldosantos" 
+    bucket         = "fiap-terraform-state-bucket-fiap" 
     key            = "networking/terraform.tfstate" # Novo caminho para o state
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-fiap-aldosantos"
+    dynamodb_table = "terraform-state-lock-fiap"
     encrypt        = true
   }
 }
