3474198254d3ca9229c67c7fb1546a882b9b254e
AldoOps <aldo.santos.oliveira95@gmail.com>
2026-02-17 21:53:34 -0300

    s3 novo

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fiap-terraform-state-bucket-fiap-fase4" 
+    bucket         = "fiap-terraform-state-bucket-fiap-hackathon" # Nome do bucket S3
     key            = "networking/terraform.tfstate" # Novo caminho para o state
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-fiap"
+    # dynamodb_table = "terraform-state-lock-fiap"
     encrypt        = true
   }
 }
